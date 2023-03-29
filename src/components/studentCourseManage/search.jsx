fafd9248d7dd086e47a10e8a605ff6551e4575a5
tsoikh0917 <103169550+tsoikh0917@users.noreply.github.com>
2023-03-29 18:46:25 +0800

    search page draft with dummy data

--- src/components/studentCourseManage/search.jsx
+++ src/components/studentCourseManage/search.jsx
@@ -1,27 +1,94 @@
 import React from "react";
 import "./search.css";
 import { FaSearch } from "react-icons/fa";
+import { useTable } from "react-table";
+import fakeData from "./MOCK_DATA.json";
 
 function Search() {
+  const data = React.useMemo(() => fakeData, []);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "course ID",
+        accessor: "course_ID",
+      },
+      {
+        Header: "course name",
+        accessor: "course_name",
+      },
+      {
+        Header: "day",
+        accessor: "day",
+      },
+      {
+        Header: "time",
+        accessor: "time",
+      },
+      {
+        Header: "place",
+        accessor: "place",
+      },
+      {
+        Header: "department",
+        accessor: "department",
+      },
+      {
+        Header: "instructor",
+        accessor: "instructor",
+      },
+      {
+        Header: "capacity",
+        accessor: "capacity",
+      },
+    ],
+    []
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
   return (
-    <div>
-      <div id="test">
-        <h1>Search Classes</h1>
+    <div id="test">
+      <h1>Search Classes</h1>
 
-        <div class="wrap">
-          <div class="search">
-            <input
-              type="text"
-              class="searchTerm"
-              placeholder="What are you looking for?"
-            ></input>
-            <button type="submit" class="searchButton">
-              <div id="icon1">
-                <FaSearch />
-              </div>
-            </button>
-          </div>
+      <div class="wrap">
+        <div class="search">
+          <input
+            type="text"
+            class="searchTerm"
+            placeholder="What are you looking for?"
+          ></input>
+          <button type="submit" class="searchButton">
+            <div id="icon1">
+              <FaSearch />
+            </div>
+          </button>
         </div>
+      </div>
+      <div>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     </div>
   );
