658732e00e39cc066f3c8b2f5b261f040c97a73d
Vincy <77875361+CDsasaku@users.noreply.github.com>
2023-03-31 14:48:24 +0800

    update profile component

--- client/src/components/studentCourseManage/profile.jsx
+++ client/src/components/studentCourseManage/profile.jsx
@@ -1,18 +1,31 @@
-import React from "react";
-function Profile() {
-  const name = "Wong Sui Ming";
-  /*"Name": "Peter Wong",
-  "ID": "115500000",
-  "Major": "Computer Science",
-  "Department": "Engineering",
-  "Year": 3*/
+import React, { useEffect, useState} from "react";
+
+function Profile() {  
+  const [name, setName] = useState(null);
+  const [id, setID] = useState(null);
+  const [major, setMajor] = useState(null);
+  const [year, setYear] = useState(null);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8080/student/3")
+      .then(res => res.json())
+      .then(
+        (data) => {
+          setName(data[0].name);
+          setID(data[0].userID);
+          setMajor(data[0].major);
+          setYear(data[0].yearOfStudy);
+        }
+      ).catch((error) => {
+        console.error(error);
+  }, [])})
+  
   return (
     <div>
       <li>Name: {name}</li>
-      <li>ID: {name}</li>
-      <li>Major: {name}</li>
-      <li>Department: {name}</li>
-      <li>Year: {name}</li>
+      <li>ID: {id}</li>
+      <li>Major: {major}</li>
+      <li>Year: {year}</li>
     </div>
   );
 }
