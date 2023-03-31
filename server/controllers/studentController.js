ac6c06fe771ef113f3656e1bd576cef55b07139e
Vincy <77875361+CDsasaku@users.noreply.github.com>
2023-03-31 10:34:59 +0800

    student api

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -1,3 +1,84 @@
 // con: connection to the database
 const con = require('../Models/mysqlModel');
 
+const getAllStudent = async (req, res) => {
+    const userID = req.param('userID');
+
+    // ? is a placeholder for a value to be inserted into the query
+    let sql = `SELECT name, userID, major, yearOfStudy
+               FROM student
+               WHERE isActive = 1`
+
+    // first parameter: sql query
+    // second parameter: array of values to be inserted into the query
+    // third parameter: callback function
+    con.query(sql, [userID], (err, result) => {
+        if (err) throw err;
+        res.status(200).send(result);
+    });
+}
+
+const getStudent = async (req, res) => {
+    const userID = req.param('userID');
+
+    let sql = `SELECT userID, major, name, email, gender, yearOfStudy, emergencyContact
+               FROM student
+               WHERE userID = ? AND isActive = 1`
+
+    con.query(sql, [userID], (err, result) => {
+        if (err) throw err;
+        res.status(200).send(result);
+    });
+}
+
+const addStudent = async (req, res) => { 
+    const data = req.body;
+
+    let sql = `INSERT INTO student
+               (major, name, email, gender, yearOfStudy, emergencyContact)
+               VALUE (?, ?, ?, ?, ?, ?)`
+
+    con.query(sql, [data.major, data.name, data.email, data.gender, data.yearOfStudy, data.emergencyContact], (err, result) => {
+        if (err) throw err;
+        res.status(201).send(result);
+    });
+}
+
+const updateStudent = async (req, res) => {
+    const userID = req.param('userID');
+    const data = req.body;
+
+    let sql = `UPDATE student SET
+               major = ?, 
+               name = ?, 
+               gender = ?, 
+               yearOfStudy = ?, 
+               emergencyContact = ?
+               WHERE userID = ?`
+
+    con.query(sql, [data.major, data.name, data.gender, data.yearOfStudy, data.emergencyContact, userID], (err, result) => {
+        if (err) throw err;
+        res.status(200).send(result);
+    });
+}
+
+const deleteStudent = async (req, res) => {
+    const userID = req.param('userID');
+
+    let sql = `UPDATE student SET
+               isActive = 0
+               WHERE userID = ?`
+
+    con.query(sql, [userID], (err, result) => {
+        if (err) throw err;
+        res.status(200).send(result);
+    });
+}
+
+module.exports = {
+    getAllStudent,
+    getStudent,
+    addStudent,
+    updateStudent,
+    deleteStudent
+}
--- server/routes/student.js
+++ server/routes/student.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const Student  = require('../controllers/studentController')
 
 const router = express.Router()
 
-
+router.get('/', Student.getAllStudent)
+router.get('/:userID', Student.getStudent)
+router.post('/', Student.addStudent)
+router.put('/:userID', Student.updateStudent)
+router.put('/delete/:userID', Student.deleteStudent)
 
 module.exports = router
