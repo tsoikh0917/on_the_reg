1a54657886b4b2d4646e32dfd5b429b98790035e
Vincy <77875361+CDsasaku@users.noreply.github.com>
2023-03-31 11:58:51 +0800

    waitlist api

--- server/controllers/waitlistController.js
+++ server/controllers/waitlistController.js
@@ -1,3 +1,56 @@
 // con: connection to the database
 const con = require('../Models/mysqlModel');
 
+const getWaitlist = async (req, res) => {
+    const userID = req.param('userID');
+
+    // ? is a placeholder for a value to be inserted into the query
+    let sql = `SELECT waitlist_number, course.courseID, courseName, description
+               FROM course_waitingList 
+               INNER JOIN course
+               ON course_waitingList.courseID = course.courseID
+               WHERE course_waitingList.userID = ?`
+
+    // first parameter: sql query
+    // second parameter: array of values to be inserted into the query
+    // third parameter: callback function
+    con.query(sql, [userID], (err, result) => {
+        if (err) throw err;
+        res.status(200).send(result);
+    });
+}
+
+const addWaitlist = async (req, res) => { 
+    const data = req.body;
+
+    let sql = `INSERT INTO course_waitingList
+               (courseID, userID, waitlist_number) VALUE 
+               (?, ?, COALESCE((SELECT max_num FROM ( 
+               SELECT MAX(waitlist_number) as max_num
+               FROM onTheRegDB.course_waitingList as num1
+               WHERE userID = ?) as num2),0) + 1);`
+
+    con.query(sql, [data.courseID, data.userID, data.userID], (err, result) => {
+        if (err) throw err;
+        res.status(201).send(result);
+    });
+}
+
+const deleteWaitlist = async (req, res) => {
+    const waitlist_number = req.param('waitlist_number');
+    const userID = req.param('userID');
+
+    let sql = `DELETE FROM course_waitingList
+               WHERE waitlist_number = ? AND userID = ?`
+
+    con.query(sql, [waitlist_number, userID], (err, result) => {
+        if (err) throw err;
+        res.status(200).send(result);
+    });
+}
+
+module.exports = {
+    getWaitlist,
+    addWaitlist,
+    deleteWaitlist
+}
--- server/routes/waitlist.js
+++ server/routes/waitlist.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const Waitlist  = require('../controllers/waitlistController')
 
 const router = express.Router()
 
+
+router.get('/:userID', Waitlist.getWaitlist)
+router.post('/', Waitlist.addWaitlist)
+router.delete('/:waitlist_number/:userID', Waitlist.deleteWaitlist)
 
 
 module.exports = router
