e93ff0a818fd9b408199ce017b924a5071650977
stanisho <78036117+stanisho@users.noreply.github.com>
2023-04-11 03:07:12 +0800

    course

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const Course  = require('../controllers/courseController')
 
 const router = express.Router()
+
+
+router.get('/', Course.ViewAllCourse)
+router.get('/:courseID', Course.ViewSpecificCourse)
+router.post('/add', Course.AddCourse)
+router.delete('/delete/:courseID', Course.DeleteCourse)
+router.put('/edit', Course.EditCourse)
 
 
 
