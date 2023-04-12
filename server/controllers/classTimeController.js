65baadeeac63c2305459ca5a547c9de711b12d23
0147123 <9536442@gmail.com>
2023-04-12 23:59:15 +0800

    Create classTimeController.js

---
+++ server/controllers/classTimeController.js
@@ -0,0 +1,17 @@
+// con: connection to the database
+const con = require('../Models/mysqlModel');
+
+const getClassTime = async (req, res) => {
+}
+
+const addClassTime = async (req, res) => { 
+}
+
+const deleteClassTime = async (req, res) => {
+}
+
+module.exports = {
+    getClassTime,
+    addClassTime,
+    deleteClassTime
+}
