fd63f8362d81296f21ecf9966acabac359597671
fythk <78533802+fythk@users.noreply.github.com>
2023-04-14 00:48:58 +0800

    update_form_css

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ function App() {
   // use here to change is user are logged in or admin
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [count, setCount] = useState(0);
-  const [isAdmin, setIsAdmin] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(true);
 
   // show different page according to the role
   function checkLogin() {
